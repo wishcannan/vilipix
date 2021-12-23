27a488c2fb767d59165586a13398061be3e8e1ab
wishcannan <49178221+wishcannan@users.noreply.github.com>
2021-12-23 17:35:33 +0800

    这次把userid的方法提交了

--- vili.py
+++ vili.py
@@ -11,6 +11,7 @@ class vili():
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36"
         }
         self.tag_api_url = 'https://www.vilipix.com/api/illust/tag/{}'#根据tag爬取
+        self.userid_api_url = 'https://www.vilipix.com/api/illust'#根据画师id爬取
         self.filename = './image/'
         if not os.path.exists(self.filename):
             os.mkdir(self.filename)
@@ -23,6 +24,7 @@ class vili():
         alist = []#申请一辆空车
         pn = 0
         n = 1
+        start_time = time.time()
         while True:
             params = {
                 'limit':30,
@@ -37,7 +39,6 @@ class vili():
             pn += 30
             n += 1
         #装填完毕
-        start_time = time.time()
         print('{}发车,这次共计{}页'.format(tag,len(alist)))
         for i in alist:
             i.start()
@@ -45,5 +46,67 @@ class vili():
             i.join()
         print('一共用时:', time.time()-start_time)
 
+    def userid_find(self,userid:int):#麻了 这个错误访问页数 他也给你正常返回200 之前写的识别方式就不能用
+        filename = self.filename + str(userid) + '/'
+        if not os.path.exists(filename):
+            os.makedirs(filename)
+        alist = []
+        pn = 0
+        #我希望一次只有最多10条线程并发运行 在之前版本里 我其实让有多少页就有多少线程并发运行 确实很快 不够显得不够
+        #优雅 我会在最后一个版本里 尝试用线程池来实现 这里呢我希望简简单单的实现一下控制并发运行数量
+        start_time = time.time()
+        while True:
+            n = 1
+            while n % 11 != 0:
+                params = {
+                    'user_id':userid,
+                    'limit':30,
+                    'offset':pn
+                }
+                r = requests.get(self.userid_api_url,params=params,headers=self.headers)
+                a = r.json()
+                if len(a['rows']) != 0:
+                    a = vili_thread.vi_thread(n,a,str(userid),filename)
+                    alist.append(a)
+                else:
+                    self.depart(userid,alist)
+                    print('一共用时:', time.time()-start_time)#最后一定会在这里结束
+                    return
+                r.close()
+                n += 1
+                pn += 30
+            self.depart(userid,alist)
+            alist = []#清空车
+            
+    
+    def depart(self,name,alist):#这个alist里面不是线程 就报错讲真
+        name = str(name)
+        print('{}发车,这次共计{}页'.format(name,len(alist)))
+        if len(alist) == 0:
+            print("是不是有鬼上车了")
+        else:
+            for i in alist:
+                i.start()
+            for i in alist:
+                i.join()
+
+    def ex(self,uid):
+        pn = 60
+        params = {
+            'user_id':uid,
+            'limit':30,
+            'offset':pn
+        }
+        r = requests.get(self.userid_api_url,params=params,headers=self.headers)
+        print(r.headers)
+        print(r.status_code)
+        print(r.text)
+
+
+
+
+
 V = vili()
-V.tag_find('黒タイツ')
+# V.tag_find('黒タイツ')
+V.userid_find(19440186)
+# V.ex(19440186)
