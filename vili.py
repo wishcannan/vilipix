fd2f3bcd017b0d91c0e8af1c91e5a811a2dd160e
wishcannan <49178221+wishcannan@users.noreply.github.com>
2021-12-23 22:00:10 +0800

    更新了 线程池运用 看起来这台机器可以接受更大 线程量 小瞧了 是我不行

--- vili.py
+++ vili.py
@@ -1,6 +1,8 @@
 import os
+from types import TracebackType
 import requests
 import vili_thread
+import pool
 import time
 
 # VI = vili_thread
@@ -12,6 +14,7 @@ class vili():
         }
         self.tag_api_url = 'https://www.vilipix.com/api/illust/tag/{}'#根据tag爬取
         self.userid_api_url = 'https://www.vilipix.com/api/illust'#根据画师id爬取
+        # self.list_api_url = 'https://www.vilipix.com/api/illust'原来 榜单和画师id公用一个api呀 就是参数不同 才发现
         self.filename = './image/'
         if not os.path.exists(self.filename):
             os.mkdir(self.filename)
@@ -77,6 +80,35 @@ class vili():
                 pn += 30
             self.depart(userid,alist)
             alist = []#清空车
+    
+    def List_find(self,mode):#mode 有daily weekly monthly
+        blist = self.get_yesterday(mode)
+        filename = self.filename + blist[0]
+        if not os.path.exists(filename):
+            os.makedirs(filename)
+        alist = []
+        start_time = time.time()
+        pn = 0  
+        while True:
+            params = {
+                'mode':mode,
+                'date':blist[1],
+                'limit':30,
+                'offset':pn
+            }
+            pn += 30
+            r = requests.get(self.userid_api_url,params=params,headers=self.headers)
+            a = r.json()
+            if len(a['rows']) != 0:
+                alist.append(a)
+            else:
+                print('准备进入池中')
+                break
+        th_pool = pool.t_pool(alist,filename)
+        th_pool.done()
+        print('一共用时:', time.time()-start_time)
+        
+
             
     
     def depart(self,name,alist):#这个alist里面不是线程 就报错讲真
@@ -89,11 +121,38 @@ class vili():
                 i.start()
             for i in alist:
                 i.join()
+    
+    def get_yesterday(self,mode='daily'):
+        #这个方法主要实现 两件事 1，为文件夹命名 2,返回昨天日期作为参数
+        a = time.time()-86400
+        yesterday = time.strftime('%Y%m%d',time.localtime(a))#这样呢 我就获取到昨天返回的其实是一个str对象
+        filename = yesterday
+        #yesterday会作为params里面参数 所以也会返回 其次也会根据mode 返回一个文件夹命名格式
+        if mode == 'weekly':
+            On_week = time.strftime('%Y%m%d',time.localtime(a - 86400 * 7))
+            filename = filename +'-'+On_week+'周榜/'
+        elif mode == 'monthly':
+            #原来写了一个判断这个月份有多少天 然后减时间的 后来感觉那个方法太长 显得不具有美感
+            year = int(yesterday[0:4])
+            month = int(yesterday[5:6])
+            alist = []
+            if(year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+                print('今天又是一个闰年呢')
+                alist = [31,29,31,30,31,30,31,31,30,31,30,31]
+            else:
+                alist = [31,28,31,30,31,30,31,31,30,31,30,31]
+            all_day = alist[(month-1 if month != 1 else 12)-1]#获取了上个月一个月有多长
+            On_month = time.strftime('%Y%m%d',time.localtime(a - 86400 * all_day))
+            filename = filename +'-'+On_month + '月榜/'
+        else:
+            filename = filename+'日榜/'
+        return [filename,yesterday]
 
     def ex(self,uid):
         pn = 60
         params = {
-            'user_id':uid,
+            'mode':'monthly',
+            'date':20211222,
             'limit':30,
             'offset':pn
         }
@@ -108,5 +167,6 @@ class vili():
 
 V = vili()
 # V.tag_find('黒タイツ')
-V.userid_find(19440186)
+# V.userid_find(19440186)
 # V.ex(19440186)
+V.List_find('weekly')
